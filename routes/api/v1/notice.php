2f82e57ae7fd8aca943a75b457514e9a3a00eed7
murato <b7350@oic.jp>
2019-10-29 17:36:10 +0900

    json渡せたよ

--- routes/api/v1/notice.php
+++ routes/api/v1/notice.php
@@ -1,3 +1,23 @@
 <?php
 
 //
+
+Route::get('/notice/liked', function () {
+
+  return [
+    'id' => 1,
+    'src_user' => [
+      'user_id' => 'XXXX',
+      'screen_name' => 'XXXX',
+      'avater' => 'XXXX'
+    ],
+    'post'=>[
+      'id' => 'XXXX',
+      'image' => 'XXXX'
+    ],
+    'like'=>[
+    'create_at' => 'XXXX'
+    ]
+  ];
+
+});
