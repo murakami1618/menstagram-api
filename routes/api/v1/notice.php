c8b30775769a310b4b40d7a104903678b44ede73
murato <b7350@oic.jp>
2019-11-01 14:11:08 +0900

    モックでぎだ

--- routes/api/v1/notice.php
+++ routes/api/v1/notice.php
@@ -21,3 +21,27 @@ Route::get('/notice/liked', function () {
   ];
 
 });
+
+Route::get('/notice/followed', function () {
+
+  return [
+    'id' => 1,
+    'src_user' => [
+      'user_id' => 'XXXX',
+      'screen_name' => 'XXXX',
+      'avater' => 'XXXX'
+    ],
+    'follow'=>[
+      'is_followed'=>false,
+      'created_at'=>'XXXX'
+    ]
+  ];
+});
+
+Route::get('/notice/system', function () {
+  return[
+    'id' =>1,
+    'text' =>'XXXX',
+    'created_at'=>'XXXX',
+  ];
+});
